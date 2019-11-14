d23e3675c3f206f64bc931d1f960da53d212f520
Darshana97 <dpbrayan1@gmail.com>
2019-11-14 11:25:40 +0530

    concatenation in dart

---
+++ concatenation in dart/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:concatenation_in_dart/concatenation_in_dart.dart' as concatenation_in_dart;
+
+main(List<String> arguments) {
+  print('Hello world: ${concatenation_in_dart.calculate()}!');
+}
---
+++ concatenation in dart/lib/concatenation_in_dart.dart
@@ -0,0 +1,11 @@
+main(List<String> arguments){
+
+  String fname = "Darshana";
+  String lname = "Senevirathna";
+  int age = 22;
+
+  print("$fname $lname is $age years old..");
+  print("Hello World I am ${fname.toUpperCase()}");
+
+
+}
