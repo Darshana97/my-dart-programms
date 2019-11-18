3cf6855fb779bcd227be9154dfd61ca93d532b41
Darshana97 <dpbrayan1@gmail.com>
2019-11-18 10:35:21 +0530

    for loop in dart

---
+++ for loop in dart/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:for_loop_in_dart/for_loop_in_dart.dart' as for_loop_in_dart;
+
+main(List<String> arguments) {
+  print('Hello world: ${for_loop_in_dart.calculate()}!');
+}
---
+++ for loop in dart/lib/for_loop_in_dart.dart
@@ -0,0 +1,19 @@
+/*main(List<String> arguments){
+
+  for(var i = 0; i < 10; i++){
+    print("Hello $i");
+  }
+
+}*/
+
+main(List<String> arguments){
+
+  for(var i = 0; i < 10; i++){
+    if(i % 2 == 0){
+      print("Helloo $i");
+    }
+
+
+  }
+
+}
