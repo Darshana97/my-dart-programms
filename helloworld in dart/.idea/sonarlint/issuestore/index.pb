0ce0b1cfea9874fa0051f0eeda0550ad16a19ba8
Darshana97 <dpbrayan1@gmail.com>
2019-11-21 07:44:46 +0530

    hello world in dart

---
+++ helloworld in dart/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+pubspec.yaml,e\8\e8e045469170189f1d6ef12576bd01af132b1018
+K
+bin/helloworld in dart.dart,7\5\7541064bf412bdc30e368d6273f532993af8657f
---
+++ helloworld in dart/bin/helloworld in dart.dart
@@ -0,0 +1,9 @@
+main(List<String> arguments){   //entry point for execution
+
+  //Variable = spot in memmory. or just a bucket to hold value
+
+  var country = "Sri lanka";
+
+  print('Hello world');
+  print(country);
+}
