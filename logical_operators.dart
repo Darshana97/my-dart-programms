0c8db3ea2dbf337ed21223039aafe52efdf4cf2b
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-12-01 17:59:29 +0530

    logical_operators

---
+++ logical_operators.dart
@@ -0,0 +1,27 @@
+main(List<String> arguments){
+
+  /*Logical operators { !, ||, &&}*/
+
+  int num1 = 100;
+  int num2 = 3;
+
+  if( !(num1 == 10) || (num2 > 0) ){    //False or True = True
+    print("Hello");
+
+  }
+  else{
+    print("Bye");
+  }
+
+
+
+  if( (num1 == 10) && (num2 > 0) ){   //False and True = False
+    print("Hello");
+
+  }
+  else{
+    print("Bye");
+  }
+
+
+}
