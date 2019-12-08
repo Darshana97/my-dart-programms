386ed330a4dea080e0b0be9ca42119b04ba484a8
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-12-08 17:45:12 +0530

    numbers_in_dart
    numbers_in_dart

---
+++ numbers_in_dart.dart
@@ -0,0 +1,13 @@
+import 'package:numbers_in_dart/numbers_in_dart.dart' as numbers_in_dart;
+main(List<String> arguments){
+
+//  num age = 22;
+//  num number = 20;
+
+  int age = 13;
+  double number = 23.34;
+
+  print(age);
+  print(number);
+
+}
