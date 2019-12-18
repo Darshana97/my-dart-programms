2318c3db4086e34f0003e1d619028ef7995cc2fa
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-12-18 11:00:08 +0530

    operators_in_dart
    operators_in_dart

---
+++ operators_in_dart.dart
@@ -0,0 +1,21 @@
+main(List<String> arguments){
+
+  int num1 = 10;
+  int num2 = 3;
+
+  var result1 = num1 + num2;
+  var result2 = num1 - num2;
+  var result3 = num1 * num2;
+  var result4 = num1 / num2;
+  var result5 = num1 % num2;
+
+
+  print(result1);
+  print(result2);
+  print(result3);
+  print(result4);
+  print(result5);
+
+
+
+}
