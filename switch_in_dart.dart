aae86759d740b428de0bd106e439a923c477a5fd
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-12-23 07:19:32 +0530

    switch_in_dart
    switch_in_dart

---
+++ switch_in_dart.dart
@@ -0,0 +1,25 @@
+main(List<String> arguments){
+
+  var age = 19;
+
+  switch(age){
+
+    case 18:
+      print("Old enough");
+      break;
+
+    case 20:
+      print("Still good");
+      break;
+
+    case 89:
+      print("Too old!");
+      break;
+
+    default:
+      print("Default $age");
+
+
+  }
+
+}
